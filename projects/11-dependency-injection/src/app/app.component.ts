e3f72134df8a9a489efbf84414050bf9bdcb6bf4
dekoher <diegoherr92@gmail.com>
2026-05-26 12:32:00 -0600

    feat: ending module 11: dependency injection and doing bonus fetching the data from an API and not using the mocked data

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from "./user-info/user-info.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  template: `
+    <h1>User Listing</h1>
+    @for (user of userData; track $index) {
+      <app-user-info [user]="user" />
+    }
+  `,
+  imports: [UserInfoComponent],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  userService = inject(UserService);
+  userData: User[] = [];
+  // constructor() {
+  // }
+  ngOnInit() {
+    this.userService.getUserData().then((data) => {
+      this.userData = data;
+      console.log('userData=>', data);
+    });
+  }
 }
--- projects/11-dependency-injection/src/app/user.service.ts
+++ projects/11-dependency-injection/src/app/user.service.ts
@@ -1,13 +1,31 @@
+import { Injectable } from '@angular/core';
 import { data, User } from './data';
 
+@Injectable({
+  providedIn: 'root'
+})
 export class UserService {
-  private userData: User[] = data;
+  private readonly userData: User[] = data;
 
   constructor() {}
 
   getUserData(): Promise<User[]> {
-    return new Promise((resolve) => {
-      resolve(this.userData);
-    });
+    return fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la red');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('data=>', data);
+        return data;
+      })
+      .catch(error => {
+        console.error('Hubo un problema:', error);
+      });
+    // return new Promise((resolve) => {
+    //   resolve(this.userData);
+    // });
   }
 }
